c5eae92fbb268fcbc5f9f061f1815be742a9f69c
channeng <channeng@hotmail.com>
2015-10-23 21:24:17 +0000

    latest

--- get_all_segmentation_events.py
+++
@@ -1,8 +0,0 @@
-from trend_30d_analysis import Mixpanel_Events_monitor
-import os
-
-api_key = os.environ["MP_PRO_KEY"]
-api_secret = os.environ["MP_PRO_SECRET"]
-monitor = Mixpanel_Events_monitor(api_key,api_secret)
-print monitor.get_all_events()
-  
--- mp/events_analysis/get_all_segmentation_events.py
+++ mp/events_analysis/get_all_segmentation_events.py
@@ -1,7 +1,9 @@
 from trend_30d_analysis import Mixpanel_Events_monitor
+import os
 
-api_key = os.environ["MP_PRO_KEY"]
-api_secret = os.environ["MP_PRO_SECRET"]
+api_key = os.environ["MP_DESKTOP_KEY"]
+api_secret = os.environ["MP_DESKTOP_SECRET"]
 monitor = Mixpanel_Events_monitor(api_key,api_secret)
-print monitor.get_all_events()
+for i in  monitor.get_all_events():
+  print i
   
